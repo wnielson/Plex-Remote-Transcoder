106e0d8a68aa533cb1177dee42fb13eadb9d9f87
Weston Nielson <wnielson@github>
2015-08-21 15:35:33 -0700

    Initial stab at load-balancing

--- prt.py
+++ prt.py
@@ -2,13 +2,16 @@
 import json
 import logging
 import logging.config
+import multiprocessing
 import os
 import pipes
+import re
 import shlex
 import shutil
 import subprocess
 import sys
 import time
+#import requests
 
 from distutils.spawn import find_executable
 
@@ -65,8 +68,10 @@ REMOTE_ARGS = ("export LD_LIBRARY_PATH=%(ld_path)s;"
                "cd %(working_dir)s;"
                "%(command)s %(args)s")
 
+LOAD_AVG_RE = re.compile(r"load averages: ([\d\.]+) ([\d\.]+) ([\d\.]+)")
+
 __author__  = "Weston Nielson <wnielson@github>"
-__version__ = "0.1.3"
+__version__ = "0.2.0"
 
 def get_config():
     path = os.path.expanduser("~/.prt.conf")
@@ -78,11 +83,27 @@ def get_config():
 def save_config(d):
     path = os.path.expanduser("~/.prt.conf")
     try:
-        json.dump(d, open(path, 'w'))
+        json.dump(d, open(path, 'w'), indent=4)
         return True
     except Exception, e:
         print "Error loading config: %s" % str(e)
     return False
+
+def get_system_load_local():
+    """
+    Returns a list of float representing the percentage load of this machine.
+    """
+    nproc = multiprocessing.cpu_count()
+    load  = os.getloadavg()
+    return [l/nproc * 100 for l in load]
+
+def get_system_load_remote(host, port, user):
+    """
+    Gets the result from ``get_system_load_local`` of a remote machine.
+    """
+    proc = subprocess.Popen(["ssh", "%s@%s" % (user, host), "-p", port, "prt", "get_load"], stdout=subprocess.PIPE)
+    proc.wait()
+    return [float(i) for i in proc.stdout.read().strip().split()]
 
 def setup_logging():
     config = get_config()
@@ -147,6 +168,20 @@ def transcode_local():
 def transcode_remote():
     setup_logging()
 
+    #session_id = None
+    #for i, v in enumerate(sys.argv[1:]):
+    #    if v == "-progressurl":
+    #        # TODO: This is crap, should use regex
+    #        #requests.put(sys.argv[1:][i+1])
+    #        session_id = sys.argv[1:][i+1].split("transcode/session/")[-1].split("/")[0]
+    #        break
+
+    #if session_id:
+    #    xml = requests.get("http://127.0.0.1:32400/status/sessions")
+    #    #dom = ET.parse(xml)
+    #    log.debug("xml = %s" % xml.text)
+   
+
     command = REMOTE_ARGS % {
         "ld_path":      "%s:$LD_LIBRARY_PATH" % LD_LIBRARY_PATH,
         "working_dir":  os.getcwd(),
@@ -160,15 +195,43 @@ def transcode_remote():
         log.info("No hosts found...using local")
         return transcode_local()
 
-    # TODO: Decide which host to use better.  For now, choose first one
-    hostname, host = config["servers"].items()[0]
+    hostname, host = None, None
+
+    if len(config["servers"]) > 1:
+        # Let's try to load-balance
+        min_load = None
+        
+        for hostname, host in config["servers"].items():
+            
+            log.debug("Getting load for host '%s'" % hostname)
+            load = get_system_load_remote(hostname, host["port"], host["user"])
+
+            log.debug("Log for '%s': %s" % (hostname, str(load)))
+
+            # XXX: Use more that just 1-minute load?
+            if min_load is None or min_load[1] > load[0]:
+                min_load = (hostname, load[0],)
+
+        # Select lowest-load host
+        log.info("Host with minimum load is '%s'" % min_load[0])
+        hostname, host = min_load[0], config["servers"][min_load[0]]
+
+    else:
+        # We don't have a choice--use the only host
+        hostname, host = config["servers"].items()[0]
+
+    log.info("Using transcode host '%s'" % hostname)
 
     # Remap the 127.0.0.1 reference to the proper address
     command.replace("127.0.0.1", config["ipaddress"])
 
+    #
+    # TODO: Remap file-path to PMS URLs
+    #
+
     args = ["ssh", "%s@%s" % (host["user"], hostname), "-p", host["port"]] + [command]
 
-    log.info("Launching transcode_remote: %s\n" % args)
+    log.info("Launching transcode_remote with args %s\n" % args)
 
     # Spawn the process
     proc = subprocess.Popen(args)
@@ -178,10 +241,12 @@ def usage():
     return
 
 def main():
-    print "Plex Remote Transcoder version %s, Copyright (C) %s\n" % (__version__, __author__)
-
     if len(sys.argv) < 2:
+        print "Plex Remote Transcoder version %s, Copyright (C) %s\n" % (__version__, __author__)
         return usage()
+
+    if sys.argv[1] == "get_load":
+        print " ".join([str(i) for i in get_system_load_local()])
 
     if sys.argv[1] == "install":
         print "Installing Plex Remote Transcoder"
@@ -209,3 +274,4 @@ def main():
 
             if save_config(config):
                 print "Host successfully added"
+
