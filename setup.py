160858b85ce460bede4edcd3489243616d1cdbe7
Liviy <liviynz@users.noreply.github.com>
2020-04-21 20:32:18 +1200

    Here's a working buggy/working python3 rewrite of the python2 version. No enhancements until the bugs are fixed. :) Known bugs : Pulling down auth token fails to login (but if you already have one in the config it connects fine). The ssh transcoder sessions seem to stick/double up for a while but still processes & runs.
    Signed-off-by: Liviy <liviynz@github>
    Signed-off-by: liviynz <andy.livingstone@gmail.com>

--- setup.py
+++ setup.py
@@ -7,15 +7,15 @@ import re
 here = path.abspath(path.dirname(__file__))
 
 def get_version():
-    return re.search("__version__ = \"([\d\.]+)\"", open("prt.py").read()).groups()[0]
+    return re.search("__version__ = \"([\d\.]+)\"", open("prt3.py").read()).groups()[0]
 
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    name='prt',
+    name='prt3',
     version=get_version(),
-    description='A remote transcoder for Plex',
+    description='A remote transcoder for Plex(3)',
     long_description=long_description,
     url='https://github.com/wnielson/Plex-Remote-Transcoder',
     author='Weston Nielson',
@@ -25,17 +25,18 @@ setup(
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
     keywords='plex media server, distributed plex, load balancing, remote transcoding',
-    py_modules=["prt"],
+    py_modules=["prt3"],
     entry_points={
         'console_scripts': [
-            'prt=prt:main',
-            'prt_local=prt:transcode_local',
-            'prt_remote=prt:transcode_remote'
+            'prt3=prt3:main',
+            'prt3_local=prt3:transcode_local',
+            'prt3_remote=prt3:transcode_remote'
         ],
     },
     install_requires=['termcolor', 'psutil']
